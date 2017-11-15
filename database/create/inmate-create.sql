de955989013d994b369a88823736423196f7a563
Robert Scanlon <scanlonr@oregonstate.edu>
2017-11-15 13:42:14 -0500

    added username and password fields to userdb table

--- database/create/inmate-create.sql
+++ database/create/inmate-create.sql
@@ -3,12 +3,14 @@
 --
 DROP TABLE IF EXISTS `inmate`;
 CREATE TABLE `inmate` (
-    `id`     int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `fname`  varchar(63)      NOT NULL,
-    `minit`  char(1)          DEFAULT NULL,
-    `lname`  varchar(63)      NOT NULL,
-    `dob`    date             NOT NULL,
-    `wallet` int(10) unsigned DEFAULT 0,
+    `id`       int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `fname`    varchar(63)      NOT NULL,
+    `minit`    char(1)          DEFAULT NULL,
+    `lname`    varchar(63)      NOT NULL,
+    `dob`      date             NOT NULL,
+    `wallet`   int(10) unsigned DEFAULT 0,
+    `username` varchar(63)      DEFAULT NULL,
+    `password` varchar(63)      DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
