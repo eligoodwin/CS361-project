50ae9c70f7851f2ee9da84d099dc3d0e48b70680
Robert Scanlon <scanlonr@oregonstate.edu>
2017-11-18 14:49:28 -0500

    code cleanup

--- gcloud-app/main.py
+++ gcloud-app/main.py
@@ -1,15 +1,35 @@
+##############################################################################
+# filename:         main.py
+#
+# author:           CS 496 Fall 2017 - Group 1:
+#                                       Eli Goodwin
+#                                       Andrew Lodge
+#                                       Robert Scanlon
+#                                       Kevin Lewis
+#
+# description:      Implements a basic admin app for the prison
+#                   E-learning system. Features include:
+#                       (1) Adding new users
+#                       (2) Deleting existing users
+#                       (3) Listing all users
+#
+# last edit:        18 November 2017
+##############################################################################
+
 import os
 import urllib
 import MySQLdb
 import jinja2
 import webapp2
 import datetime
-
 import json
 import random
 import string
 
-BASEURL = "https://cs361project.appspot.com/"
+BASEURL   = "https://cs361project.appspot.com/"
+HOME_LINK = BASEURL
+ALL_LINK  = BASEURL + "all"
+ADD_LINK  = BASEURL + "prisoner"
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -21,6 +41,11 @@ CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
 CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 
+
+##############################################################################
+# returns MySQLdb obj connected to the g cloud database
+# code taken from Google App Engine Python Documentation
+##############################################################################
 def connect_to_cloudsql():
     # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
     # will be set to 'Google App Engine/version'.
@@ -48,155 +73,198 @@ def connect_to_cloudsql():
 
     return db
 
+
+##############################################################################
+# Renders the application home page with links to
+# (1) add user
+# (2) show all users
+##############################################################################
+def renderHomePage(self):
+    nav = {}
+    nav['homelink'] = HOME_LINK 
+    nav['homelinktext'] = "Home"
+    nav['newuserlink'] = ADD_LINK 
+    nav['newuserlinktext'] = "Add User"
+    nav['alluserslink'] = ALL_LINK 
+    nav['alluserslinktext'] = "All Users"
+    template = JINJA_ENVIRONMENT.get_template('MainPage.html')
+    self.response.write(template.render(nav=nav))
+
+
+##############################################################################
+# Shows all users in the database in a table
+##############################################################################
+def showAllUsers(self):
+    nav = {}
+    nav['homelink'] = HOME_LINK 
+    nav['homelinktext'] = "Home"
+    nav['newuserlink'] = ADD_LINK 
+    nav['newuserlinktext'] = "Add User"
+    nav['alluserslink'] = ALL_LINK 
+    nav['alluserslinktext'] = "All Users"
+
+    db = connect_to_cloudsql()
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM inmate')
+
+    users = []
+    for row in cursor:
+        user = {}
+        user['id'] = int(row[0])
+        user['fname'] = str(row[1])
+        user['minit'] = str(row[2])
+        user['lname'] = str(row[3])
+        user['dob'] = str(row[4])
+        user['wallet'] = int(row[5])
+        user['username'] = str(row[6])
+        user['password'] = str(row[7])
+        user['remove_link'] = BASEURL + "prisoner/" + str(row[0])
+        users.append(user)
+   
+    template = JINJA_ENVIRONMENT.get_template('allusers.html')
+    self.response.write(template.render(users=users, nav=nav))
+
+
+##############################################################################
+# Renders the page where a user can be added
+# the fields id, username, and password are auto generated
+##############################################################################
+def renderAddUser(self):
+   nav = {}
+   nav['homelink'] = HOME_LINK 
+   nav['homelinktext'] = "Home"
+   nav['alluserslink'] = ALL_LINK 
+   nav['alluserslinktext'] = "All Users"
+   template = JINJA_ENVIRONMENT.get_template('adduser.html')
+   self.response.write(template.render(nav=nav))
+
+
+##############################################################################
+# adds a new user to the database
+# the new user information is passed in the request body
+##############################################################################
+def addNewUser(self):
+    db = connect_to_cloudsql()
+
+    inmateToAdd = {}
+    inmateToAdd['fname'] = self.request.get('fname')
+    inmateToAdd['minit'] = self.request.get('minit')
+    inmateToAdd['lname'] = self.request.get('lname')
+    inmateToAdd['dob'] = self.request.get('dob')
+
+    inmateToAdd['username'] = str(inmateToAdd['fname']) + \
+                              str(inmateToAdd['minit'])+ \
+                              str(inmateToAdd['lname'])
+    inmateToAdd['password'] = ''.join(random.choice(string.ascii_uppercase + \
+                                                    string.digits) \
+                                                    for _ in range(12))
+    inmateToAdd['wallet'] = int(self.request.get('wallet'))
+
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO inmate (fname, minit, lname, dob, " + \
+                   "username, password, wallet) VALUES " + \
+                   "(%s, %s, %s, %s, %s, %s, %s);",
+                   (inmateToAdd['fname'], inmateToAdd['minit'],
+                    inmateToAdd['lname'], inmateToAdd['dob'],
+                    inmateToAdd['username'], inmateToAdd['password'],
+                    inmateToAdd['wallet']))
+
+    db.commit()
+    db.close()
+
+    nav = {}
+    nav['homelink'] = HOME_LINK 
+    nav['homelinktext'] = "Home"
+    nav['newuserlink'] = ADD_LINK 
+    nav['newuserlinktext'] = "Add User"
+    nav['alluserslink'] = ALL_LINK 
+    nav['alluserslinktext'] = "All Users"
+
+    mess = {}
+    mess['success'] = "User Added to Database"
+
+    template = JINJA_ENVIRONMENT.get_template('addsuccess.html')
+    self.response.write(template.render(nav=nav, mess=mess))
+
+
+##############################################################################
+# renders the page which asks the user to confirm they wish to
+# delete the user with ID = 'id'
+##############################################################################
+def renderConfirmDelete(self, id):
+    nav = {}
+    nav['homelink'] = HOME_LINK 
+    nav['homelinktext'] = "Home"
+    nav['newuserlink'] = ADD_LINK 
+    nav['newuserlinktext'] = "Add User"
+    nav['alluserslink'] = ALL_LINK 
+    nav['alluserslinktext'] = "All Users"
+    mess = {}
+    mess['warning'] = "Are you sure you want to delete the " + \
+            "user with ID: " + str(id) + "?"
+    mess['buttonText'] = "Yes"
+    link = {}
+    link['deleteLink'] = BASEURL + "prisoner/" + str(id) 
+    template = JINJA_ENVIRONMENT.get_template('confirmDelete.html')
+    self.response.write(template.render(mess=mess, link=link, nav=nav))
+
+
+##############################################################################
+# removes the user with ID = 'id' from the database then
+# renders the page confirming the delete took place
+##############################################################################
+def deleteUser(self, id):
+    db = connect_to_cloudsql()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM inmate WHERE id = %s", str(id))
+    db.commit()
+    db.close()
+
+    mess = {}
+    mess['header'] = "User Removed from Database"
+
+    nav = {}
+    nav['homelink'] = HOME_LINK 
+    nav['homelinktext'] = "Home"
+    nav['newuserlink'] = ADD_LINK 
+    nav['newuserlinktext'] = "Add User"
+    nav['alluserslink'] = ALL_LINK 
+    nav['alluserslinktext'] = "All Users"
+
+    template = JINJA_ENVIRONMENT.get_template('deleteSuccess.html')
+    self.response.write(template.render(nav=nav, mess=mess))
+
+
+# [START RequestHandlers]
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        nav = {}
-        nav['homelink'] = BASEURL
-        nav['homelinktext'] = "Home"
-        nav['newuserlink'] = BASEURL + "prisoner"
-        nav['newuserlinktext'] = "Add User"
-        nav['alluserslink'] = BASEURL + "all"
-        nav['alluserslinktext'] = "All Users"
-        template = JINJA_ENVIRONMENT.get_template('MainPage.html')
-        self.response.write(template.render(nav=nav))
-
+        renderHomePage(self)
 
 class ShowAll(webapp2.RequestHandler):
     def get(self):
+        showAllUsers(self)
 
-        nav = {}
-        nav['homelink'] = BASEURL
-        nav['homelinktext'] = "Home"
-        nav['newuserlink'] = BASEURL + "prisoner"
-        nav['newuserlinktext'] = "Add User"
-        nav['alluserslink'] = BASEURL + "all"
-        nav['alluserslinktext'] = "All Users"
-
-        db = connect_to_cloudsql()
-        cursor = db.cursor()
-        cursor.execute('SELECT * FROM inmate')
-
-        users = []
-        for row in cursor:
-            i = int(row[0])
-            f = str(row[1])
-            m = str(row[2])
-            l = str(row[3])
-            d = str(row[4])
-            w = int(row[5])
-            u = str(row[6])
-            p = str(row[7])
-            user = {}
-            user['id'] = i
-            user['fname'] = f
-            user['minit'] = m
-            user['lname'] = l
-            user['dob'] = d
-            user['wallet'] = w
-            user['username'] = u
-            user['password'] = p
-            user['remove_link'] = BASEURL + "prisoner/" + str(i)
-            users.append(user)
-       
-        template = JINJA_ENVIRONMENT.get_template('allusers.html')
-        self.response.write(template.render(users=users, nav=nav))
 
 class Add(webapp2.RequestHandler):
     def get(self):
-       nav = {}
-       nav['homelink'] = BASEURL
-       nav['homelinktext'] = "Home"
-       nav['alluserslink'] = BASEURL + "all"
-       nav['alluserslinktext'] = "All Users"
-       template = JINJA_ENVIRONMENT.get_template('adduser.html')
-       self.response.write(template.render(nav=nav))
-
+        renderAddUser(self)
 
     def post(self):
-            """This end point is used to add an inmate user to the database"""
-            #connect to database
-            db = connect_to_cloudsql()
+        addNewUser(self)
 
-            inmateToAdd = {}
-            inmateToAdd['fname'] = self.request.get('fname')
-            inmateToAdd['minit'] = self.request.get('minit')
-            inmateToAdd['lname'] = self.request.get('lname')
-            inmateToAdd['dob'] = self.request.get('dob')
-
-            inmateToAdd['username'] = str(inmateToAdd['fname']) + str(inmateToAdd['minit'])+ str(inmateToAdd['lname'])
-            inmateToAdd['password'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12))
-            inmateToAdd['wallet'] = int(self.request.get('wallet'))
-
-            cursor = db.cursor()
-            cursor.execute("INSERT INTO inmate (fname, minit, lname, dob, username, password, wallet) VALUES (%s, %s, %s, %s, %s, %s, %s);",
-                           (inmateToAdd['fname'], inmateToAdd['minit'], inmateToAdd['lname'],
-                            inmateToAdd['dob'], inmateToAdd['username'],
-                            inmateToAdd['password'], inmateToAdd['wallet']))
-
-            db.commit()
-            db.close()
-
-            nav = {}
-            nav['homelink'] = BASEURL
-            nav['homelinktext'] = "Home"
-            nav['newuserlink'] = BASEURL + "prisoner"
-            nav['newuserlinktext'] = "Add User"
-            nav['alluserslink'] = BASEURL + "all"
-            nav['alluserslinktext'] = "All Users"
-
-            mess = {}
-            mess['success'] = "User Added to Database"
-
-            template = JINJA_ENVIRONMENT.get_template('addsuccess.html')
-            self.response.write(template.render(nav=nav, mess=mess))
-            return
 
 class Remove(webapp2.RequestHandler):
     def get(self, id=None):
         if not id:
             return
+        renderConfirmDelete(self, id)
 
-        nav = {}
-        nav['homelink'] = BASEURL
-        nav['homelinktext'] = "Home"
-        nav['newuserlink'] = BASEURL + "prisoner"
-        nav['newuserlinktext'] = "Add User"
-        nav['alluserslink'] = BASEURL + "all"
-        nav['alluserslinktext'] = "All Users"
-        mess = {}
-        mess['warning'] = "Are you sure you want to delete the " + \
-                "user with ID: " + str(id) + "?"
-        mess['buttonText'] = "Yes"
-        link = {}
-        link['deleteLink'] = BASEURL + "prisoner/" + str(id) 
-        template = JINJA_ENVIRONMENT.get_template('confirmDelete.html')
-        self.response.write(template.render(mess=mess, link=link, nav=nav))
 
     def post(self, id=None):
         if not id:
             return
-
-        db = connect_to_cloudsql()
-        cursor = db.cursor()
-        cursor.execute("DELETE FROM inmate WHERE id = %s", str(id))
-        db.commit()
-        db.close()
-
-        mess = {}
-        mess['header'] = "User Removed from Database"
-
-        nav = {}
-        nav['homelink'] = BASEURL
-        nav['homelinktext'] = "Home"
-        nav['newuserlink'] = BASEURL + "prisoner"
-        nav['newuserlinktext'] = "Add User"
-        nav['alluserslink'] = BASEURL + "all"
-        nav['alluserslinktext'] = "All Users"
-
-        template = JINJA_ENVIRONMENT.get_template('deleteSuccess.html')
-        self.response.write(template.render(nav=nav, mess=mess))
-
-        
+        deleteUser(self, id)
+# [END RequestHandlers]
 
 # [START app]
 app = webapp2.WSGIApplication([
